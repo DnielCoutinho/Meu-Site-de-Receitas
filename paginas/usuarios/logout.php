05d4721b7adcdc9215168e53b4dc29cd9af938f9
DnielCoutinho <162729233+DnielCoutinho@users.noreply.github.com>
2025-09-24 14:09:14 -0300

    Delete paginas directory

--- paginas/usuarios/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once('../../config.php'); // Adicionado para ter acesso à constante BASE_URL
-session_destroy();
-header("Location: " . BASE_URL . "/"); // Redireciona para a página principal
-exit();
-?>
